b6e987d9b2b5f77055a58e1ab28e368ce123ad5d
Ivan Diaz <idiaz3@gmu.edu>
2015-03-04 11:25:04 -0500

    Added framework for the race level state

---
+++ js/raceLevel.js
@@ -0,0 +1,19 @@
+state.raceLevel =  function (game) {
+
+};
+
+state.raceLevel.prototype = {
+
+    preload: function(){
+    },
+    create:function(){
+	/*DEBUG SHIT PLEASE REMOVE WHEN YOU GET HERE*/
+	this.debugText = this.add.text(10, this.world.centerY,"THIS HAS YET TO BE IMPLEMENTED,\nVOTE ME ON STEAM\nAND YOU GET FREE DLC\nWHICH INCLUDES THIS LEVEL\n#DLC4LYFE, #ONDATISH, #ARNOLDPALMER", {size:"16px", fill: "#FFF", align: "left"});
+
+	//DEBUG END
+    },
+    upload: function(){
+
+    }
+
+}
